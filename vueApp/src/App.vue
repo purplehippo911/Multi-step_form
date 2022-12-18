b4a3345beaa3a03ca5dcabf6f8cff3591d6ddf6e
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-12-18 16:52:39 +0100

    started styling FormProcess🎨

--- vueApp/src/App.vue
+++ vueApp/src/App.vue
@@ -5,9 +5,13 @@ import ConfirmationButtons  from "./components/ConfirmationButtons.vue";
 </script>
 
 <template>
-    <main>
+    <header>
         <div class="container row">
             <FormProcess/>
+        </div>
+    </header>
+    <main>
+        <div class="container row">
             <RouterView/>
             <ConfirmationButtons/>
         </div>
@@ -15,7 +19,7 @@ import ConfirmationButtons  from "./components/ConfirmationButtons.vue";
 </template>
 
 <style lang="scss">
-main {
+main, header {
    .row {
        flex-direction:column;
        section {
@@ -30,9 +34,15 @@ main {
 }
 
 .container {
-    width:80%;
+    width:90%;
     margin:0 auto;
     gap:2rem;
+}
+
+header {
+    .row {
+        flex-direction:column;
+    }
 }
 
 section {
@@ -45,17 +55,30 @@ section {
     padding:5rem;
     background:url("./assets/images/bg-sidebar-mobile.svg") no-repeat;
     background-size:cover;
-    @media screen and (min-width: 700px) {
-        background:url("./assets/images/bg-sidebar-desktop.svg") no-repeat;
+    .container {
+        width:100%;
     }
     .row {
-        justify-content:center;
         .steps-wrapper {
             width:20%;
+            .steps__count {
+                padding:.5em;
+                border:1px solid $White;
+                border-radius:20px;
+                font-weight:700;
+            }
             .steps__text, .steps__title{
                 display:none;
             }
-            @media screen and (min-width: 700px) {
+        }
+    }
+    @media screen and (min-width: 700px) {
+        background:url("./assets/images/bg-sidebar-desktop.svg") no-repeat;
+        .row {
+            flex-direction:column;
+            align-content:flex-start;
+            .steps-wrapper {
+                display:flex;
                 .steps__text, .steps__title{
                     display:block;
                 }
--- vueApp/src/components/FormProcess.vue
+++ vueApp/src/components/FormProcess.vue
@@ -6,25 +6,25 @@
     <div class="container row">
       
       <div class="steps-wrapper">
-          <a class="steps__count" href="#">1</a>
+          <a class="steps__count" href="/">1</a>
           <p class="steps__text">Step 1</p>
           <h3 class="steps__title">Your info</h3>
       </div>
       
       <div class="steps-wrapper">
-          <a class="steps__count" href="#">2</a>
+          <a class="steps__count" href="/step-2">2</a>
           <p class="steps__text">Step 2</p>
           <h3 class="steps__title">Select Plan</h3>
       </div>
       
       <div class="steps-wrapper">
-          <a class="steps__count" href="#">3</a>
+          <a class="steps__count" href="/step-3">3</a>
           <p class="steps__text">Step 3</p>
           <h3 class="steps__title">Add-Ons</h3>
       </div>
       
       <div class="steps-wrapper">
-          <a class="steps__count" href="#">4</a>
+          <a class="steps__count" href="/step-4">4</a>
           <p class="steps__text">Step 4</p>
           <h3 class="steps__title">Summary</h3>
       </div>
